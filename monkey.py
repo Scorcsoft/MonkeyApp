f87c5273029dcd446f90adc32efbe2874a346c5e
Wang Man <wangman@scorcsoft.com>
2024-08-07 09:31:21 +0800

    bugfix：无配置时报错问题

--- monkey.py
+++ monkey.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+
 try:
     import rumps
     import PyQt5
@@ -14,12 +15,11 @@ except ModuleNotFoundError as e:
 
 import ScorcsoftCore.config as config
 
-
 if sys.platform == 'darwin':
     import Mac.MacMonkey as MacMonkey
 else:
     print('现在只能在Mac上使用，辣鸡Windows的还没来得及写，过几天再说！')
-
+    sys.exit()
 
 if os.path.isfile('config.json'):
     try:
@@ -27,8 +27,13 @@ if os.path.isfile('config.json'):
         config.Settings = json.loads(fp.read())
         fp.close()
     except:
+        config.Settings = {"countdown_note": "", "start_time": "08:20", "end_time": "16:40", "percent_note": "",
+                           "percent_display": 1, "wage_note": "", "wage_symbol": "", "wage": 0, "wage_display": 1}
         print('加载本地配置失败')
-        pass
+else:
+    config.Settings = {"countdown_note": "", "start_time": "08:20", "end_time": "16:40", "percent_note": "",
+                       "percent_display": 1, "wage_note": "", "wage_symbol": "", "wage": 0, "wage_display": 1}
+    print('没有配置文件，请重新设置。')
 
 monkeyASCII = r'''
             __,__
